17efe5f1b03ed5e0e80a15831e8777c6e009be0a
Andrés Saam <andressaa.dev@gmail.com>
2019-12-05 23:18:45 -0500

    Connecting to my postgres database and sending its data

--- app.js
+++ app.js
@@ -1,14 +1,48 @@
 const express = require("express");
 const app = express();
-
 const port = 8080;
+
+const dbConfig = {
+  "host": "localhost",
+  "port": 5432,
+  "database": "postgres",
+  "user": "postgres",
+  "password": "andressaa94"
+};
+
+const initOptions = {
+  // global event notification;
+  error(error, e) {
+    if (e.cn) {
+      // A connection-related error;
+      //
+      // Connections are reported back with the password hashed,
+      // for safe errors logging, without exposing passwords.
+      console.log("CN:", e.cn);
+      console.log("EVENT:", error.message || error);
+    }
+  }
+};
+
+const pgp = require("pg-promise")(initOptions);
+const db = pgp(dbConfig);
 
 // Starting the server
 app.listen(port, () => {
   console.log("Listening on port 8080...");
 });
 
-// Creating API
+// Creating API endpoints
 app.get("/api/hello_world", (req, res) => {
   res.send("Hello world");
+});
+
+app.get("/api/jobs", (req, res) => {
+  db.any("SELECT * FROM jobs", [true])
+    .then(data => {
+      res.json(data);
+    })
+    .catch(error => {
+      console.log("Error: ", error);
+    });
 });
